815f65609eac440c492c69ae4e66f0cbc5021166
Chomp <dev@dev.sp-tarkov.com>
2025-06-10 17:29:20 +0100

    Updated 13.0, improved its code quality to same level as 13.1 and some minor changes to 13.1

--- 13.1AddTraderWithDynamicAssorts/AddTraderWithDynamicAssorts.cs
+++ 13.1AddTraderWithDynamicAssorts/AddTraderWithDynamicAssorts.cs
@@ -19,15 +19,15 @@ public record ModMetadata : AbstractModMetadata
 {
     public override string Name { get; set; } = "AddTraderWithDynamicAssortsExample";
     public override string Author { get; set; } = "SPTarkov";
-    public override List<string>? Contributors { get; set; }
+    public override List<string>? Contributors { get; set; } = ["Clodan", "CWX"];
     public override string Version { get; set; } = "1.0.0";
     public override string SptVersion { get; set; } = "4.0.0";
     public override List<string>? LoadBefore { get; set; }
     public override List<string>? LoadAfter { get; set; }
     public override List<string>? Incompatibilities { get; set; }
     public override Dictionary<string, string>? ModDependencies { get; set; }
-    public override string? Url { get; set; }
-    public override bool? IsBundleMod { get; set; }
+    public override string? Url { get; set; } = "https://github.com/sp-tarkov/server-mod-examples";
+    public override bool? IsBundleMod { get; set; } = false;
     public override string? Licence { get; set; } = "MIT";
 }
 
--- 13AddTraderWithAssortJson/AddTraderWithAssortJson.cs
+++ 13AddTraderWithAssortJson/AddTraderWithAssortJson.cs
@@ -5,104 +5,82 @@ using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Spt.Config;
 using SPTarkov.Server.Core.Models.Spt.Mod;
-using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Routers;
 using SPTarkov.Server.Core.Servers;
-using SPTarkov.Server.Core.Services;
-using SPTarkov.Server.Core.Utils.Cloners;
+using SPTarkov.Server.Core.Utils;
 using Path = System.IO.Path;
 
 namespace _13AddTraderWithAssortJson;
 
+// This record holds the various properties for your mod
 public record ModMetadata : AbstractModMetadata
 {
     public override string Name { get; set; } = "AddTraderWithAssortJsonExample";
     public override string Author { get; set; } = "SPTarkov";
-    public override List<string>? Contributors { get; set; }
+    public override List<string>? Contributors { get; set; } = ["Clodan", "CWX"];
     public override string Version { get; set; } = "1.0.0";
     public override string SptVersion { get; set; } = "4.0.0";
-    public override List<string>? LoadBefore { get; set; }
-    public override List<string>? LoadAfter { get; set; }
-    public override List<string>? Incompatibilities { get; set; }
+    public override List<string>? LoadBefore { get; set; } = ["AddTraderWithDynamicAssortsExample"];
+    public override List<string>? LoadAfter { get; set; } = ["RegisterClassesInDIExample"];
+    public override List<string>? Incompatibilities { get; set; } = ["ReadJsonConfigExample"];
     public override Dictionary<string, string>? ModDependencies { get; set; }
-    public override string? Url { get; set; }
-    public override bool? IsBundleMod { get; set; }
+    public override string? Url { get; set; } = "https://github.com/sp-tarkov/server-mod-examples";
+    public override bool? IsBundleMod { get; set; } = false;
     public override string? Licence { get; set; } = "MIT";
 }
 
+/// <summary>
+/// Feel free to use this as a base for your mod
+/// </summary>
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
-public class AddTraderWithAssortJson : IOnLoad
+public class AddTraderWithAssortJson(
+    ModHelper modHelper,
+    ImageRouter imageRouter,
+    ConfigServer configServer,
+    TimeUtil timeUtil,
+    AddCustomTraderHelper addCustomTraderHelper // This is a custom class we add for this mod, we made it injectable so it can be accessed like other classes here
+)
+    : IOnLoad
 {
-    private readonly ISptLogger<AddTraderWithAssortJson> _logger;
-    private readonly ModHelper _modHelper;
-    private readonly DatabaseService _databaseService;
-    private readonly ImageRouter _imageRouter;
-    private readonly ConfigServer _configServer;
-    private readonly ICloner _cloner;
-    private readonly TraderConfig _traderConfig;
-    private readonly RagfairConfig _ragfairConfig;
+    private readonly TraderConfig _traderConfig = configServer.GetConfig<TraderConfig>();
+    private readonly RagfairConfig _ragfairConfig = configServer.GetConfig<RagfairConfig>();
 
-    public AddTraderWithAssortJson(
-        ISptLogger<AddTraderWithAssortJson> logger,
-        ModHelper modHelper,
-        DatabaseService databaseService,
-        ImageRouter imageRouter,
-        ConfigServer configServer,
-        ICloner cloner)
-    {
-        _logger = logger;
-        _modHelper = modHelper;
-        _databaseService = databaseService;
-        _imageRouter = imageRouter;
-        _configServer = configServer;
-        _cloner = cloner;
-
-        _traderConfig = _configServer.GetConfig<TraderConfig>();
-        _ragfairConfig = _configServer.GetConfig<RagfairConfig>();
-    }
 
     public Task OnLoad()
     {
-        var pathToMod = _modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        // A path to the mods files we use below
+        var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
 
+        // A relative path to the trader icon to show
         var traderImagePath = Path.Combine(pathToMod, "db/cat.jpg");
-        var traderBase = _modHelper.GetJsonDataFromFile<TraderBase>(pathToMod, "db/base.json");
 
-        var assort = _modHelper.GetJsonDataFromFile<TraderAssort>(pathToMod, "db/assort.json");
+        // The base json containing trader settings we will add to the server
+        var traderBase = modHelper.GetJsonDataFromFile<TraderBase>(pathToMod, "db/base.json");
 
-        // Create helper class and use it to register our traders image/icon + set its stock refresh time
-        var addTraderHelper = new AddTraderHelper();
-        _imageRouter.AddRoute(traderBase.Avatar.Replace(".jpg", ""), traderImagePath);
-        addTraderHelper.SetTraderUpdateTime(_traderConfig, traderBase, 3600, 4000);
+        // Create a helper class and use it to register our traders image/icon + set its stock refresh time
+        imageRouter.AddRoute(traderBase.Avatar.Replace(".jpg", ""), traderImagePath);
+        addCustomTraderHelper.SetTraderUpdateTime(_traderConfig, traderBase, timeUtil.GetHoursAsSeconds(1), timeUtil.GetHoursAsSeconds(2));
 
-        // Add trader to flea market
-        _ragfairConfig.Traders[traderBase.Id] = true;
+        // Add our trader to the config file, this lets it be seen by the flea market
+        _ragfairConfig.Traders.TryAdd(traderBase.Id, true);
 
-        // Add new trader to the trader dictionary in DatabaseServer - this is where the assort json is loaded
-        /*
-         * The assortJSON includes the following:
-         * Milk available for roubles at LL1
-         * Milk available for item barter at LL1
-         * Helmet w/ soft armour available for roubles at LL1
-         * Helmet w/ soft armour available for item barter at LL1
-         *
-         * It is *REQUIRED* to use MongoIDs for IDs in the assort
-         */
-        addTraderHelper.AddTraderToDb(
-            traderBase,
-            _databaseService.GetTables(),
-            _cloner,
-            assort);
+        // Add our trader (with no items yet) to the server database
+        // An 'assort' is the term used to describe the offers a trader sells, it has 3 parts to an assort
+        // 1: The item
+        // 2: The barter scheme, cost of the item (money or barter)
+        // 3: The Loyalty level, what rep level is required to buy the item from trader
+        addCustomTraderHelper.AddTraderWithEmptyAssortToDb(traderBase);
 
-        addTraderHelper.AddTraderToLocales(
-            traderBase,
-            _databaseService.GetTables(),
-            traderBase.Name,
-            "Cat",
-            traderBase.Nickname,
-            traderBase.Location,
-            "This is the cat shop. Meow.");
-        
+        // Add localisation text for our trader to the database so it shows to people playing in different languages
+        addCustomTraderHelper.AddTraderToLocales(traderBase, "Cat", "This is the cat shop. Meow.");
+
+        // Get the assort data from JSON
+        var assort = modHelper.GetJsonDataFromFile<TraderAssort>(pathToMod, "db/assort.json");
+
+        // Save the data we loaded above into the trader we've made
+        addCustomTraderHelper.OverwriteTraderAssort(traderBase.Id, assort);
+
+        // Send back a success to the server to say our trader is good to go
         return Task.CompletedTask;
     }
 }
